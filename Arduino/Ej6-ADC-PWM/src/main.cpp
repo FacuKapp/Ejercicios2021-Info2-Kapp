e7409396e7098b9bc0a568b70c3665e83335a7eb
Facundo Kapp <facukapp3668@gmail.com>
2021-05-04 01:14:23 -0300

    Reacomodo de archivos

--- Arduino/Ej6-ADC-PWM/src/main.cpp
+++ Arduino/Ej6-ADC-PWM/src/main.cpp
@@ -1,9 +1,26 @@
+/* Realizar la medición de un sensor conectado al ADC y producir una variación proporcional en la salida PWM. Ejemplos:
+• Potenciómetro que regule el brillo de un LED, la velocidad de un motor o ventilador.
+• LDR que regule el brillo de un LED.
+• Sensor de temperatura que regules la velocidad de un ventilador. */
+
 #include <Arduino.h>
 
+#define Sensor A0                   //Pin lectura sensor analogica
+#define Motor 5                       //Pin Led salida analogica (PWM)
+
+int LecturaSensor = 0;              //Variable para guardar el valor de la lectura
+
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
+    pinMode( Sensor, INPUT );       //Sensor como entrada, aunque no hace falta
+    pinMode( Motor, OUTPUT );         //Led como salida
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+    LecturaSensor = analogRead( Sensor );           //Lectura del valor de la entrada
+
+    LecturaSensor = (LecturaSensor/4);              ///Ya que la entrada analogica toma valores de 0 a 1023
+    ;                                               //y la salida PWM solo de 0 a 255
+    analogWrite( Motor, LecturaSensor );            //Escritura en la salida PWM
 }
