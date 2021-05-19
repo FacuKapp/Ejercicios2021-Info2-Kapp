6b41824d3ebaabe809ccdf3c0b4b1e7bbcfda8a1
Facundo Kapp <facukapp3668@gmail.com>
2021-05-18 23:23:09 -0300

    Programa funcionando

--- Arduino/Ej10-Display7Segmentos/src/main.cpp
+++ Arduino/Ej10-Display7Segmentos/src/main.cpp
@@ -14,10 +14,10 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   int i = 0;          //Contador
-  PORTD = B00000000;
 
-  for(i = 0; i <= 8; i++)
+  for(i = 7; i >= 0; i--)             //Por la conexion el el Tinkercad, inicia del 7 al 0
   {
+    PORTD = B00000000;				        //Apaga todo el puerto D completo
     PORTD = PORTD | (1<<i);           //Mueve el bit en 1 por cada vuelta del for
     delay(TIEMPO);                    //Espera un tiempo
   }
